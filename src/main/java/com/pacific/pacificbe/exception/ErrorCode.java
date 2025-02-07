2df883fd23dbdf94a156d0d90b319e9d256aa4b8
yuno.k <120995031+Khang1z2t@users.noreply.github.com>
2025-02-07 20:29:56 +0700

    hot fix enum ErrorCode

--- src/main/java/com/pacific/pacificbe/exception/ErrorCode.java
+++ src/main/java/com/pacific/pacificbe/exception/ErrorCode.java
@@ -6,21 +6,21 @@ import org.springframework.http.HttpStatusCode;
 
 @Getter
 public enum ErrorCode {
-    UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error"),
-    INVALID_KEY(1001, "Uncategorized error"),
-    USER_EXISTED(1002, "User existed"),
-    USERNAME_INVALID(1003, "Username must be at least 3 characters"),
-    INVALID_PASSWORD(1004, "Password must be at least 8 characters"),
-    USER_NOT_EXISTED(1005, "User not existed"),
-    UNAUTHENTICATED(1006, "Unauthenticated"),
-    UNAUTHORIZED(1007, "You don't have permission"),
+    UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR),
+    INVALID_KEY(1001, "Uncategorized error", HttpStatus.BAD_REQUEST),
+    INVALID_USERNAME(1003, "Username must be at least {min} characters", HttpStatus.BAD_REQUEST),
+    INVALID_PASSWORD(1004, "Password must be at least {min} characters", HttpStatus.BAD_REQUEST),
+    UNAUTHENTICATED(1006, "Unauthenticated", HttpStatus.UNAUTHORIZED),
+    UNAUTHORIZED(1007, "You do not have permission", HttpStatus.FORBIDDEN),
     ;
 
-    ErrorCode(int code, String message) {
+    ErrorCode(int code, String message, HttpStatusCode statusCode) {
         this.code = code;
         this.message = message;
+        this.statusCode = statusCode;
     }
 
     private final int code;
+    private final HttpStatusCode statusCode;
     private final String message;
 }
