4531de7b74526a00dab3aab2e45049254a4931b7
yuno.k <120995031+Khang1z2t@users.noreply.github.com>
2025-04-02 17:45:29 +0700

    hot fix booking scheduler and add otp scheduler

---
+++ src/main/java/com/pacific/pacificbe/scheduler/OtpScheduler.java
@@ -0,0 +1,22 @@
+package com.pacific.pacificbe.scheduler;
+
+import com.pacific.pacificbe.model.Otp;
+import com.pacific.pacificbe.repository.OtpRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class OtpScheduler {
+    private final OtpRepository otpRepository;
+
+    @Scheduled(fixedRate = 300000)
+    public void updateOtp() {
+        // Xóa tất cả OTP đã hết hạn
+        otpRepository.deleteByExpiresAtBefore(Instant.now());
+    }
+}
