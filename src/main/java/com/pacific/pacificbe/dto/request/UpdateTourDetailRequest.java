0ec5466e9681acfbd0c3742a505ec927229a4ab0
Seiri5521 <dannyvu001@gamil.com>
2025-04-19 14:54:10 +0700

    api update tour detail v2 :v

---
+++ src/main/java/com/pacific/pacificbe/dto/request/UpdateTourDetailRequest.java
@@ -0,0 +1,34 @@
+package com.pacific.pacificbe.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UpdateTourDetailRequest {
+    @Schema(description = "Ngày bắt đầu tour", example = "2025-12-01")
+    LocalDateTime startDate;
+    @Schema(description = "Ngày kết thúc tour", example = "2025-12-03")
+    LocalDateTime endDate;
+    @Schema(description = "Giá tour cho người lớn", example = "1000000")
+    BigDecimal priceAdults;
+    @Schema(description = "Giá tour cho trẻ em", example = "500000")
+    BigDecimal priceChildren;
+    @Schema(description = "Số lượng người", example = "10")
+    Integer quantity;
+    @Schema(description = "Id của tour", example = "1")
+    String tourDetailId;
+    @Schema(description = "id của hdv", example = "1")
+    String guideId;
+    @Schema(description = "Id của khách sạn", example = "1")
+    String hotelId;
+    @Schema(description = "Id của phương tiện", example = "1")
+    String transportId;
+}
