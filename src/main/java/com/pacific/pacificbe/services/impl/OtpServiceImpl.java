600d8962aaeed1b09a8b04f79566c9e98124e36e
yuno.k <120995031+Khang1z2t@users.noreply.github.com>
2025-04-26 19:56:27 +0700

    optimize otp and delete entity otp

--- src/main/java/com/pacific/pacificbe/services/impl/OtpServiceImpl.java
+++ src/main/java/com/pacific/pacificbe/services/impl/OtpServiceImpl.java
@@ -2,50 +2,50 @@ package com.pacific.pacificbe.services.impl;
 
 import com.pacific.pacificbe.exception.AppException;
 import com.pacific.pacificbe.exception.ErrorCode;
-import com.pacific.pacificbe.model.Otp;
+
 import com.pacific.pacificbe.model.User;
-import com.pacific.pacificbe.repository.OtpRepository;
 import com.pacific.pacificbe.repository.UserRepository;
 import com.pacific.pacificbe.services.OtpService;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.Instant;
-import java.util.Optional;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import static com.pacific.pacificbe.utils.Constant.OTP_KEY;
 
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(level = lombok.AccessLevel.PRIVATE, makeFinal = true)
 public class OtpServiceImpl implements OtpService {
-    OtpRepository otpRepository;
     UserRepository userRepository;
+    private final StringRedisTemplate stringRedisTemplate;
 
     @Override
     @Transactional
     public String generateOtp(String email) {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
-        otpRepository.deleteByEmail(user.getEmail());
-
         String otp = String.format("%06d", new Random().nextInt(999999));
-        Otp otpEntity = new Otp();
-        otpEntity.setEmail(user.getEmail());
-        otpEntity.setOtpCode(otp);
-        otpEntity.setExpiresAt(Instant.now().plusSeconds(60 * 30));
-        otpRepository.save(otpEntity);
+        String key = OTP_KEY + user.getEmail();
+        stringRedisTemplate.opsForValue().set(
+                key,
+                otp,
+                30 * 60,
+                TimeUnit.SECONDS
+        );
         return otp;
     }
 
     @Override
     @Transactional
     public boolean verifyOtp(String email, String otp) {
-        Optional<Otp> otpOptional = otpRepository.findByEmailAndExpiresAtAfter(email, Instant.now());
-
-        if (otpOptional.isPresent() && otpOptional.get().getOtpCode().equals(otp)) {
-            otpRepository.delete(otpOptional.get());
+        String storedOtp = stringRedisTemplate.opsForValue().get(OTP_KEY + email);
+        if (storedOtp != null && storedOtp.equals(otp)) {
+            stringRedisTemplate.delete(OTP_KEY + email);
             return true;
         }
         return false;
